b0d59c54e8142facbd930b8506791394ab6807a9
Tiina Koskiranta <tiina.koskiranta@gmail.com>
2023-12-22 09:48:23 +0200

    update top card layout

--- src/components/devices/card.tsx
+++ src/components/devices/card.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { LatestReadingResponse } from '../../api/types';
-import { Body } from '../styled/typography';
+import { Body, Caption2Light } from '../styled/typography';
 import { Tag } from '../styled/tag';
 import { getTimeAgoString } from '../../utils/datetime';
 import { DateTime } from 'luxon';
-import { Box } from '../styled/box';
 import { Reading } from '../styled/readings';
+import { Flex } from '../styled/flex';
 
 const StyledDeviceCard = styled.div`
   border: 1px solid ${({ theme }) => theme.colors.borders.secondary};
@@ -15,12 +15,18 @@ const StyledDeviceCard = styled.div`
   box-shadow: ${({ theme }) => theme.colors.shadows.boxShadow};
 `;
 const TopCard = styled.div`
-  padding: ${({ theme }) => theme.spacings.s16};
-  border-bottom: 1px solid ${({ theme }) => theme.colors.borders.secondary};
+  display: block;
+  padding-top: ${({ theme }) => theme.spacings.s16};
+`;
 
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-start;
+const TopGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+`;
+
+const BorderedFlex = styled(Flex)`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.borders.secondary};
+  align-items: center;
 `;
 
 const BottomCard = styled.div`
@@ -41,46 +47,57 @@ export const DeviceCard = ({
   return (
     <StyledDeviceCard>
       <TopCard>
-        <Box mt="s8">
-          <Body>{latestData.name}</Body>
+        <Flex justifyContent="flex-end" px="s8" mb="s8">
           <Tag
             variant={tagVariant}
             text={getTimeAgoString(latestData.created_at)}
           />
-        </Box>
-        <Box>
-          {latestData.humidity && (
-            <Reading value={latestData.humidity} unit="humidity" />
-          )}
-        </Box>
-        <Box>
-          {latestData.pressure && (
-            <Reading value={latestData.pressure} unit="pressure" />
-          )}
-        </Box>
-        <Box>
-          {latestData.temperature && (
-            <Reading value={latestData.temperature} unit="temperature" />
-          )}
-          {latestData.max_temperature && (
-            <Reading
-              value={latestData.max_temperature}
-              unit="temperature"
-              variant="max"
-              sizeVariant="small"
-            />
-          )}
-          {latestData.min_temperature && (
-            <Reading
-              value={latestData.min_temperature}
-              unit="temperature"
-              variant="min"
-              sizeVariant="small"
-            />
-          )}
-        </Box>
+        </Flex>
+        <TopGrid>
+          <BorderedFlex pl="s16">
+            <Body>{latestData.name}</Body>
+          </BorderedFlex>
+          <BorderedFlex justifyContent="flex-start">
+            {latestData.humidity && (
+              <Reading value={latestData.humidity} unit="humidity" />
+            )}
+          </BorderedFlex>
+          <BorderedFlex justifyContent="center">
+            {latestData.pressure && (
+              <Reading value={latestData.pressure} unit="pressure" />
+            )}
+          </BorderedFlex>
+          <BorderedFlex pr="s16" justifyContent="flex-end">
+            {latestData.temperature && (
+              <Reading value={latestData.temperature} unit="temperature" />
+            )}
+          </BorderedFlex>
+          <Flex pt="s16" pl="s16">
+            <Caption2Light>Day</Caption2Light>
+          </Flex>
+          <Flex flexDirection="column" pt="s16"></Flex>
+          <Flex flexDirection="column" pt="s16" alignItems="center"></Flex>
+          <Flex flexDirection="column" pt="s16" pr="s16" alignItems="flex-end">
+            {latestData.max_temperature && (
+              <Reading
+                value={latestData.max_temperature}
+                unit="temperature"
+                variant="max"
+                sizeVariant="small"
+              />
+            )}
+            {latestData.min_temperature && (
+              <Reading
+                value={latestData.min_temperature}
+                unit="temperature"
+                variant="min"
+                sizeVariant="small"
+              />
+            )}
+          </Flex>
+        </TopGrid>
       </TopCard>
-      <BottomCard>Graph not yet implemented</BottomCard>
+      <BottomCard />
     </StyledDeviceCard>
   );
 };
